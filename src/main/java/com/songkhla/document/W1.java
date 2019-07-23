b8f2f0c7e4b251f27b8394408ca6163743e67fc6
Petpilin <Petpilin@LAPTOP-N8VHQIM6>
2019-07-23 13:49:12 +0700

    23/07/62 #1

--- src/main/java/com/songkhla/document/W1.java
+++ src/main/java/com/songkhla/document/W1.java
@@ -186,7 +186,7 @@ public class W1 {
 					.load(new java.io.File("D:/TEMPLATE/w1.docx"));
 			processVariable(bookmarkvalue,wordMLPackage);
 			processTABLE(bookmarkvalue,wordMLPackage);
-			wordMLPackage.save(new java.io.File("D:/สำนวนอิเล็กทรอนิกส์"+"/"+PoliceStationName+"/ปี"+ccYear+"/"+casetype+cs+"-"+ccYear+"/บันทึกการตรวจสำนวนการสอบสวน"+cs+"-"+ccYear+".doc"));
+			wordMLPackage.save(new java.io.File("D:/สำนวนอิเล็กทรอนิกส์"+"/"+PoliceStationName+"/ปี"+ccYear+"/"+casetype+"/"+casetype+cs+"-"+ccYear+"/บันทึกการตรวจสำนวนการสอบสวน"+cs+"-"+ccYear+".doc"));
 		}catch( Exception ex) {
 			ex.printStackTrace();
 		}
--- src/main/java/com/songkhla/document/W3.java
+++ src/main/java/com/songkhla/document/W3.java
@@ -203,7 +203,7 @@ public class W3 {
 					.load(new java.io.File("D:/TEMPLATE/w3.docx"));
 			processVariable(bookmarkvalue,wordMLPackage);
 			processTABLE(bookmarkvalue,wordMLPackage);
-			wordMLPackage.save(new java.io.File("D:/สำนวนอิเล็กทรอนิกส์"+"/"+PoliceStationName+"/ปี"+ccYear+"/"+casetype+cs+"-"+ccYear+"/หนังสือส่งสำนวนคดีสั่งฟ้อง(ผู้ต้องหาหลบหนี)"+cs+"-"+ccYear+".doc"));
+			wordMLPackage.save(new java.io.File("D:/สำนวนอิเล็กทรอนิกส์"+"/"+PoliceStationName+"/ปี"+ccYear+"/"+casetype+"/"+casetype+cs+"-"+ccYear+"/หนังสือส่งสำนวนคดีสั่งฟ้อง(ผู้ต้องหาหลบหนี)"+cs+"-"+ccYear+".doc"));
 		}catch( Exception ex) {
 			ex.printStackTrace();
 		}
