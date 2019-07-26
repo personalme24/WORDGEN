de56f54993dee2fc815306e86bb2db3b51b07480
Petpilin <Petpilin@LAPTOP-N8VHQIM6>
2019-07-26 20:55:45 +0700

    26/07/62 #4

--- src/main/java/com/songkhla/wordgen/DateLabelFormatter.java
+++ src/main/java/com/songkhla/wordgen/DateLabelFormatter.java
@@ -8,6 +8,7 @@ package com.songkhla.wordgen;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.TimeZone;
 import javax.swing.JFormattedTextField;
 import javax.swing.JOptionPane;
@@ -19,7 +20,7 @@ import javax.swing.JOptionPane;
 public class DateLabelFormatter  extends JFormattedTextField.AbstractFormatter {
 
 	    private String datePattern = "d/MM/yyyy";
-	    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+	    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern, new Locale("th", "TH"));
 
 	    @Override
 	    public Object stringToValue(String text) throws ParseException {
@@ -40,12 +41,15 @@ public class DateLabelFormatter  extends JFormattedTextField.AbstractFormatter {
 	    @Override
 	    public String valueToString(Object value) throws ParseException {
 	        if (value != null) {
-	            Calendar cal = (Calendar) value;
+	            Calendar cal = (Calendar) value;  
 	            String text_tmp = dateFormatter.format(cal.getTime());
-	            String[] text_split = text_tmp.split("/");
+                    return text_tmp;
+	            /*
+                    String[] text_split = text_tmp.split("/");
 		    	int year = Integer.parseInt(text_split[2]);
 		    	year+=543;
 	            return text_split[0]+"/"+text_split[1]+"/"+year;
+                    */
 	        }
 	        return "";
 	    }
